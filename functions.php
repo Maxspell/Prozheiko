f99277f3d38d712dc02218e19cb4b3985f267589
Max Stizhko <m.stizhko@gmail.com>
2024-10-30 20:25:53 +0200

    clear files

--- functions.php
+++ functions.php
@@ -49,7 +49,7 @@ function prozheiko_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'prozheiko' ),
+			'header-menu' => esc_html__( 'Header Menu', 'prozheiko' ),
 		)
 	);
 
@@ -70,21 +70,6 @@ function prozheiko_setup() {
 		)
 	);
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'prozheiko_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-
 	/**
 	 * Add support for core custom logo.
 	 *
@@ -103,56 +88,12 @@ function prozheiko_setup() {
 add_action( 'after_setup_theme', 'prozheiko_setup' );
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function prozheiko_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'prozheiko_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'prozheiko_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function prozheiko_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'prozheiko' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'prozheiko' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'prozheiko_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function prozheiko_scripts() {
 	wp_enqueue_style( 'prozheiko-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'prozheiko-style', 'rtl', 'replace' );
-
-	wp_enqueue_script( 'prozheiko-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'prozheiko_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
@@ -163,16 +104,4 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
 

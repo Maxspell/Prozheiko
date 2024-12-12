1d8a490d699040cb90d6e2366986a1968b5f0647
Max Stizhko <m.stizhko@gmail.com>
2024-12-13 01:10:35 +0200

    Add clinic-offers section

--- templates/clinic_offers.php
+++ templates/clinic_offers.php
@@ -10,27 +10,69 @@
             <div class="clinic-offers__content">
                 <ul class="clinic-offers__list">
                     <li class="clinic-offers__item">
-                        <div class="clinic-offers__item-title">Металеві</div>
-                        <div class="clinic-offers__item-price">28 000 грн</div>
-                        <div class="clinic-offers__item-description">Цей варіант має досить просту конструкцію, що складається з кількох замків, що кріпляться на поверхні зубів</div>
+                        <div class="clinic-offers__item-icon">
+                            <span>01</span>
+                            <svg class="clinic-offers-icon" aria-hidden="true">
+                                <use href="/wp-content/themes/prozheiko/assets/icons/icons.svg#card-logo"></use>
+                            </svg>
+                        </div>
+                        <div class="clinic-offers__item-content">
+                            <div class="clinic-offers__item-head">
+                                <div class="clinic-offers__item-title">Металеві</div>
+                                <div class="clinic-offers__item-price"><span>28 000</span> грн</div>
+                            </div>
+                            <div class="clinic-offers__item-description">Цей варіант має досить просту конструкцію, що складається з кількох замків, що кріпляться на поверхні зубів</div>
+                        </div>
                     </li>
                     <li class="clinic-offers__item">
-                        <div class="clinic-offers__item-title">Металеві</div>
-                        <div class="clinic-offers__item-price">28 000 грн</div>
-                        <div class="clinic-offers__item-description">Цей варіант має досить просту конструкцію, що складається з кількох замків, що кріпляться на поверхні зубів</div>
+                        <div class="clinic-offers__item-icon">
+                            <span>01</span>
+                            <svg class="clinic-offers-icon" aria-hidden="true">
+                                <use href="/wp-content/themes/prozheiko/assets/icons/icons.svg#card-logo"></use>
+                            </svg>
+                        </div>
+                        <div class="clinic-offers__item-content">
+                            <div class="clinic-offers__item-head">
+                                <div class="clinic-offers__item-title">Металеві</div>
+                                <div class="clinic-offers__item-price"><span>28 000</span> грн</div>
+                            </div>
+                            <div class="clinic-offers__item-description">Цей варіант має досить просту конструкцію, що складається з кількох замків, що кріпляться на поверхні зубів</div>
+                        </div>
                     </li>
                     <li class="clinic-offers__item">
-                        <div class="clinic-offers__item-title">Металеві</div>
-                        <div class="clinic-offers__item-price">28 000 грн</div>
-                        <div class="clinic-offers__item-description">Цей варіант має досить просту конструкцію, що складається з кількох замків, що кріпляться на поверхні зубів</div>
+                        <div class="clinic-offers__item-icon">
+                            <span>01</span>
+                            <svg class="clinic-offers-icon" aria-hidden="true">
+                                <use href="/wp-content/themes/prozheiko/assets/icons/icons.svg#card-logo"></use>
+                            </svg>
+                        </div>
+                        <div class="clinic-offers__item-content">
+                            <div class="clinic-offers__item-head">
+                                <div class="clinic-offers__item-title">Металеві</div>
+                                <div class="clinic-offers__item-price"><span>28 000</span> грн</div>
+                            </div>
+                            <div class="clinic-offers__item-description">Цей варіант має досить просту конструкцію, що складається з кількох замків, що кріпляться на поверхні зубів</div>
+                        </div>
                     </li>
                     <li class="clinic-offers__item">
-                        <div class="clinic-offers__item-title">Металеві</div>
-                        <div class="clinic-offers__item-price">28 000 грн</div>
-                        <div class="clinic-offers__item-description">Цей варіант має досить просту конструкцію, що складається з кількох замків, що кріпляться на поверхні зубів</div>
+                        <div class="clinic-offers__item-icon">
+                            <span>01</span>
+                            <svg class="clinic-offers-icon" aria-hidden="true">
+                                <use href="/wp-content/themes/prozheiko/assets/icons/icons.svg#card-logo"></use>
+                            </svg>
+                        </div>
+                        <div class="clinic-offers__item-content">
+                            <div class="clinic-offers__item-head">
+                                <div class="clinic-offers__item-title">Металеві</div>
+                                <div class="clinic-offers__item-price"><span>28 000</span> грн</div>
+                            </div>
+                            <div class="clinic-offers__item-description">Цей варіант має досить просту конструкцію, що складається з кількох замків, що кріпляться на поверхні зубів</div>
+                        </div>
                     </li>
                 </ul>
-                <div class="clinic-offers__image"></div>
+                <div class="clinic-offers__image">
+                    <img src="/wp-content/themes/prozheiko/assets/img/clinic-offers.svg" alt="">
+                </div>
             </div>
         </div>
     </div>

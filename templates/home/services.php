023c14d63dd15a3cb01f1d8b57a694581503ee48
Maxspell <m.stizhko@gmail.com>
2024-12-16 13:54:20 +0200

    Add logic services

--- templates/home/services.php
+++ templates/home/services.php
@@ -1,187 +1,58 @@
+<?php $services = get_field( 'services' ); ?>
+<?php if ( !$services['disabled'] ) { ?>
 <section class="services">
     <div class="container">
         <div class="services__header section-header">
             <div class="services__label section-label">
-              <span class="label-text">Послуги</span>
+                <span class="label-text"><?= $services['label'] ?></span>
             </div>
-            <h2 class="services__title section-title">Ваша усмішка<br><span>в надійних руках</span></h2>
+            <h2 class="services__title section-title"><?= $services['title'] ?></h2>
         </div>
-        <ul class="services__list">
-      
-            <li class="services__item">
-                <div class="services__item-meta">
-                    <div class="services__item-icon">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bleaching-icon.svg" alt="">
-                    </div>
-                    <div class="services__item-num">01</div>
-                </div>
+        <?php if ( !empty( $services['service_categories'] ) ) { ?>
+            <ul class="services__list">
+                <?php foreach ( $services['service_categories'] as $index => $term ) { ?>
+                    <li class="services__item">
+                        <div class="services__item-meta">
+                            <div class="services__item-icon">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bleaching-icon.svg" alt="">
+                            </div>
+                            <div class="services__item-num">0<?= $index + 1 ?></div>
+                        </div>
+                        <h3 class="services__item-title"><?= esc_html( $term->name ) ?></h3>
 
-                <h3 class="services__item-title">Гігієна та відбілювання</h3>
-                <ul class="services__item-list">
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Гігієна зубів</span>
-                        <span class="services__item-detail-price">від 2 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Відбілювання зубів</span>
-                        <span class="services__item-detail-price">від 3 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Плазмоліфтинг</span>
-                        <span class="services__item-detail-price">від 4 000 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Лікування ясен</span>
-                        <span class="services__item-detail-price">від 5 000 грн</span>
-                    </li>
-                </ul>
-            </li>
+                        <?php 
+                        $args = array(
+                            'post_type' => 'service',
+                            'posts_per_page' => -1,
+                            'tax_query' => array(
+                                array(
+                                    'taxonomy' => 'service_category',
+                                    'field'    => 'term_id',
+                                    'terms'    => $term->term_id,
+                                ),
+                            ),
+                        );
+                        $query = new WP_Query( $args );
 
-            <li class="services__item">
-                <div class="services__item-meta">
-                    <div class="services__item-icon">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bleaching-icon.svg" alt="">
-                    </div>
-                    <div class="services__item-num">01</div>
-                </div>
-
-                <h3 class="services__item-title">Гігієна та відбілювання</h3>
-                <ul class="services__item-list">
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Гігієна зубів</span>
-                        <span class="services__item-detail-price">від 2 500 грн</span>
+                        if ( $query->have_posts() ) : ?>
+                            <ul class="services__item-list">
+                                <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                                    <li class="services__item-detail">
+                                        <span class="services__item-detail-name"><?php the_title(); ?></span>
+                                        <span class="services__item-detail-price">
+                                            <?= get_field( 'price' ) ? 'від ' . esc_html( get_field( 'price' ) ) . ' грн' : ''; ?>
+                                        </span>
+                                    </li>
+                                <?php endwhile; ?>
+                            </ul>
+                        <?php 
+                        endif; 
+                        wp_reset_postdata(); 
+                        ?>
                     </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Відбілювання зубів</span>
-                        <span class="services__item-detail-price">від 3 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Плазмоліфтинг</span>
-                        <span class="services__item-detail-price">від 4 000 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Лікування ясен</span>
-                        <span class="services__item-detail-price">від 5 000 грн</span>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="services__item">
-                <div class="services__item-meta">
-                    <div class="services__item-icon">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bleaching-icon.svg" alt="">
-                    </div>
-                    <div class="services__item-num">01</div>
-                </div>
-
-                <h3 class="services__item-title">Гігієна та відбілювання</h3>
-                <ul class="services__item-list">
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Гігієна зубів</span>
-                        <span class="services__item-detail-price">від 2 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Відбілювання зубів</span>
-                        <span class="services__item-detail-price">від 3 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Плазмоліфтинг</span>
-                        <span class="services__item-detail-price">від 4 000 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Лікування ясен</span>
-                        <span class="services__item-detail-price">від 5 000 грн</span>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="services__item">
-                <div class="services__item-meta">
-                    <div class="services__item-icon">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bleaching-icon.svg" alt="">
-                    </div>
-                    <div class="services__item-num">01</div>
-                </div>
-
-                <h3 class="services__item-title">Гігієна та відбілювання</h3>
-                <ul class="services__item-list">
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Гігієна зубів</span>
-                        <span class="services__item-detail-price">від 2 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Відбілювання зубів</span>
-                        <span class="services__item-detail-price">від 3 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Плазмоліфтинг</span>
-                        <span class="services__item-detail-price">від 4 000 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Лікування ясен</span>
-                        <span class="services__item-detail-price">від 5 000 грн</span>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="services__item">
-                <div class="services__item-meta">
-                    <div class="services__item-icon">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bleaching-icon.svg" alt="">
-                    </div>
-                    <div class="services__item-num">01</div>
-                </div>
-
-                <h3 class="services__item-title">Гігієна та відбілювання</h3>
-                <ul class="services__item-list">
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Гігієна зубів</span>
-                        <span class="services__item-detail-price">від 2 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Відбілювання зубів</span>
-                        <span class="services__item-detail-price">від 3 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Плазмоліфтинг</span>
-                        <span class="services__item-detail-price">від 4 000 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Лікування ясен</span>
-                        <span class="services__item-detail-price">від 5 000 грн</span>
-                    </li>
-                </ul>
-            </li>
-
-            <li class="services__item">
-                <div class="services__item-meta">
-                    <div class="services__item-icon">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bleaching-icon.svg" alt="">
-                    </div>
-                    <div class="services__item-num">01</div>
-                </div>
-
-                <h3 class="services__item-title">Гігієна та відбілювання</h3>
-                <ul class="services__item-list">
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Гігієна зубів</span>
-                        <span class="services__item-detail-price">від 2 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Відбілювання зубів</span>
-                        <span class="services__item-detail-price">від 3 500 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Плазмоліфтинг</span>
-                        <span class="services__item-detail-price">від 4 000 грн</span>
-                    </li>
-                    <li class="services__item-detail">
-                        <span class="services__item-detail-name">Лікування ясен</span>
-                        <span class="services__item-detail-price">від 5 000 грн</span>
-                    </li>
-                </ul>
-            </li>
-      
-        </ul>
+                <?php } ?>
+            </ul>
+        <?php } ?>
     </div>
 </section>
+<?php } ?>
